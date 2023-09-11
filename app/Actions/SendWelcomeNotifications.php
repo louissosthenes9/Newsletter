6b70dea12f4f4bee82c7cfdf601119b5d991defd
unknown <louissosthenes9@gmail.com>
2023-09-11 09:11:34 +0300

    new changes

--- app/Actions/SendWelcomeNotifications.php
+++ app/Actions/SendWelcomeNotifications.php
@@ -1,10 +1,19 @@
 <?php
 namespace App\Actions;
 
-Class SendWelcomeNotification{
+use Closure;
+use App\Notifications\WelcomeNotification;
+
+Class SendWelcomeNotifications
+{
 
 
+      public function handle($payload,Closure $next)
+      {
+           $subscriber=$payload['subscriber'];
+           $subscriber->notify(new WelcomeNotification);
+           
+           return $next($payload);
+      }
 
-
-    
 }
